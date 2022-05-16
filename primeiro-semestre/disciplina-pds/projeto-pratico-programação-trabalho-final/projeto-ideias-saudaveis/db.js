1e4c69b7ae42c55cf20e375dd55be65ff538fe09
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-05-16 18:51:06 -0300

    Applying clean code principles

--- primeiro-semestre/disciplina-pds/projeto-pratico-programação-trabalho-final/projeto-ideias-saudaveis/db.js
+++ primeiro-semestre/disciplina-pds/projeto-pratico-programação-trabalho-final/projeto-ideias-saudaveis/db.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose()
-// const db = new sqlite3.Database('./db/ideias-saudaveis.db')
-const db = new sqlite3.Database('./db/ideias-saudaveis-vazio.db')
+const db = new sqlite3.Database('./db/ideias-saudaveis.db')
+// const db = new sqlite3.Database('./db/ideias-saudaveis-vazio.db')
  
 db.serialize(function(){
 
