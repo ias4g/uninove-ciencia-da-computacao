fb332c085c3dcc00275aa3f0accec287f99f98d1
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-05-17 21:53:37 -0300

    Create vetores-ordenação-2022-05-17.c

---
+++ primeiro-semestre/disciplina-pds/Vetore-ordenação/vetores-ordenação-2022-05-17.c
@@ -0,0 +1,42 @@
+/**
+ * @file vetores-ordenação-2022-05-17.c
+//  * @author Izael Silva (https//github.com/Ias4g)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-17
+ *
+ * @copyright Copyright (c) 2022
+ * Imprimindo lista das letras
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#define SIZE 5
+
+int main()
+{
+    system("color f");
+    setlocale(LC_ALL, "Portuguese");
+
+    int i;             // controle do laço
+    char letras[SIZE]; // recebe 10 caracteres
+
+    for (i = 0; i < SIZE; i++)
+    {
+        printf("Digite a %2dª letra: ", i + 1);
+        fflush(stdin);
+        scanf("%c", &letras[i]); // lê e guarda o valor
+    }
+    printf("\n\n");
+    printf("-. Lista das letras .-\n");
+    printf("-----------------------\n");
+
+    for (i = 0; i < SIZE; i++)
+    { // laço para imprimir o valor
+        printf("%2dª Letra: %c\n", i + 1, letras[i]);
+    }
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
