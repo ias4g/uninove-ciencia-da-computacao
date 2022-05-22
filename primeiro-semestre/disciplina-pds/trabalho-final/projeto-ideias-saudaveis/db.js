86af1cb0e98d25fc11307265d949310e63eeec13
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-05-21 21:22:58 -0300

    update

---
+++ primeiro-semestre/disciplina-pds/trabalho-final/projeto-ideias-saudaveis/db.js
@@ -0,0 +1,38 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./db/ideias-saudaveis.db')
+// const db = new sqlite3.Database('./db/ideias-saudaveis-vazio.db')
+
+db.serialize(function () {
+
+    //Criando a tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT,
+            description TEXT,
+            link TEXT,
+            createdAt TEXT
+        );
+    `)
+
+
+
+
+
+    //Consultando dados na tabela
+    // db.all(`SELECT* FROM ideas`, function(err, rows){
+    //     if (err) return console.log(err)
+    //     console.log(rows)
+    // })
+
+
+    //Deletando dados da tabela
+    // db.run(`DELETE FROM ideas`, function(err){
+    //     if (err) return console.log(err)
+    //     console.log("Deletei", this)
+    // })
+})
+
+module.exports = db
