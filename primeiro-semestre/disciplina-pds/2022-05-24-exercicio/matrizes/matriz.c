70172d294daecb06c1baf49f5d287640b1168d6d
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-05-24 21:26:52 -0300

    Create matriz.c

---
+++ primeiro-semestre/disciplina-pds/2022-05-24-exercicio/matrizes/matriz.c
@@ -0,0 +1,27 @@
+/**
+ * @file matriz.c
+ * @author Izael Silva (https://gitub.com/Ias4g)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-24
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+#define MATRIZ_L 2
+#define MATRIZ_C 3
+
+int main()
+{
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    printf("\n\n");
+    system("pause");
+    return 0;
+}
