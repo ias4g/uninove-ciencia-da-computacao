95aa0b6bb089af60aba927e7091ba6c5545827aa
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-05-30 15:43:42 -0300

    Create tabuada.c

---
+++ primeiro-semestre/disciplina-app/tabuada/tabuada.c
@@ -0,0 +1,42 @@
+/**
+ * @file tabuada.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 0.1
+ * @date 2022-05-30
+ *
+ * @copyright Copyright (c) 2022
+ *
+ * FAZER UM ALGORITMO EM LINGUAGEM C PARA EXIBIR NA TELA A TABUADA ESCOLHIDA PELO USUARIO.
+ * USE A ESTRUTURA WHILE(). Trabalhe as formatações de saída.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    system("color A");
+
+    int i;
+    int number;
+    char operacao;
+
+    printf("Digite o numero para fazer a operacao: \n");
+    scanf("%i", &number);
+
+    printf("Digite a operacao [+][-][*][/]: \n");
+    fflush(stdin);
+    gets(operacao);
+
+    printf("%s\n", operacao);
+
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%i x %i = %i\n", number, i, number * i);
+    }
+
+    return 0;
+}
