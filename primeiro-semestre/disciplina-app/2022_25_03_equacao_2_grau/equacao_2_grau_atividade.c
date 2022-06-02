0486f932f162639ac32bca3188a636e5c73000e4
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-02 20:10:51 -0300

    Create equacao_2_grau_atividade.c

---
+++ primeiro-semestre/disciplina-app/2022_25_03_equacao_2_grau/equacao_2_grau_atividade.c
@@ -0,0 +1,44 @@
+/**
+ * Elabore um Algoritmo para resolver uma equação do 2º grau.
+ * O usuario deve informar os coeficientes: a,b,c.
+ *
+ * Profª: Debora Virgilia Canne
+ * Data postagem: 11 de março de 2022
+ *
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    system("color A");
+
+    float a;
+    float b;
+    float c;
+    float x1;
+    float x2;
+    float delta;
+
+    printf("\n\nf(x): ax2 + bx + c\n\n");
+
+    printf("Digite o valar para o coeficiente A: ");
+    scanf("%f", &a);
+
+    printf("Digite o valar para o coeficiente B: ");
+    scanf("%f", &b);
+
+    printf("Digite o valar para o termo independente C: ");
+    scanf("%f", &c);
+
+    system("cls");
+
+    printf("\n\nFuncao a ser resolvida -> f(x): %4.1fx2 + %4.1fx + %4.1f", a, b, c);
+
+    return 0;
+}
