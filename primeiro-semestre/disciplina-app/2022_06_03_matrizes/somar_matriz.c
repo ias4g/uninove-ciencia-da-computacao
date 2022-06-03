507fea3be3bdef8e42f3a95a1e917d69bf287233
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-03 19:45:56 -0300

    Create somar_matriz.c

---
+++ primeiro-semestre/disciplina-app/2022_06_03_matrizes/somar_matriz.c
@@ -0,0 +1,19 @@
+/**
+ * Desenvolver um programa em linguagem c
+ * para exibir a soma das linhas de uma matriz
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    system("cls");
+    system("color A");
+
+    system("pause");
+    return 0;
+}
