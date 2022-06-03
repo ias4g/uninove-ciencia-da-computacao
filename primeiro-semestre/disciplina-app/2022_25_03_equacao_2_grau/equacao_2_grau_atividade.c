fb441a9db72b707e46e2e734585ceb9a5b1b5f9f
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-02 21:43:24 -0300

    Update equacao_2_grau_atividade.c

--- primeiro-semestre/disciplina-app/2022_25_03_equacao_2_grau/equacao_2_grau_atividade.c
+++ primeiro-semestre/disciplina-app/2022_25_03_equacao_2_grau/equacao_2_grau_atividade.c
@@ -43,11 +43,10 @@ int main()
     printf("\n\n");
 
     delta = (b * b) - (4 * a * c);
-    printf("%f", delta);
 
     if (delta < 0)
     {
-        printf("\nDelta = %f\nNeste caso delta não possui raizes reais.\n\n", delta);
+        printf("\nDelta = %.2f\nNeste caso delta nao possui raizes reais.\n\n", delta);
     }
 
     system("pause");
