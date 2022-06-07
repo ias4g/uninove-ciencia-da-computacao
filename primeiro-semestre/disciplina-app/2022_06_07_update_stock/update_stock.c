dfb40c705fb345e82c7a9cf26b130cb123d79aa5
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 20:20:27 -0300

    Update update_stock.c

--- primeiro-semestre/disciplina-app/2022_06_07_update_stock/update_stock.c
+++ primeiro-semestre/disciplina-app/2022_06_07_update_stock/update_stock.c
@@ -1,12 +1,12 @@
 /**
  * @file update_stock.c
  * @author Izael Silva (https://github.com/Ias4g)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-06-07
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  * Fazer um programa em c
  * para cadstrar 5 produtos
  * e atualizar o estoque.
@@ -23,8 +23,17 @@ int main()
     system("color A");
     setlocale(LC_ALL, "Portuguese");
 
-    char product[5][100];
+    int i;
     int stock[5][2];
+    char product[5][100];
+    // Estoque com 2 colunas [0] ataul e [1] minimo
+
+    printf("Cadastre o nome do produto: \n");
+    for (i = 0; i < 5; i++)
+    {
+        printf("Produto: ");
+        setbuf(stdin, NULL);
+    }
 
     system("pause");
     return 0;
