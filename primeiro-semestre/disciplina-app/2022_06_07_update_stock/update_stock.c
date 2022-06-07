18d86fe7927e44d40fa8d9c4641a1df6009953e9
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 20:15:47 -0300

    Create update_stock.c

---
+++ primeiro-semestre/disciplina-app/2022_06_07_update_stock/update_stock.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+
+int main()
+{
+    system("cls");
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    char product[5][100];
+    int stock[5][2];
+
+    system("pause");
+    return 0;
+}
