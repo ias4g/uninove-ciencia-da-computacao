4f3e7630f916833daef11462369fce29310214b8
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 20:15:58 -0300

    Create matrizes.c

---
+++ primeiro-semestre/disciplina-app/2022_06_07_revisao/matrizes.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+
+int main()
+{
+    system("cls");
+    system("color A");
+
+    int x = 25;
+    int y = 5;
+    int z = 0;
+
+    for (z = 0; x >= y; x = x - y, z++)
+    {
+        printf("x=%2d, y=%2d, z=%2d\n", x, y, z);
+    }
+
+    printf("------------------\n\n");
+
+    for (z = 0; x >= y; x = x - y, z++); // ESTE PONTO E VIRGULA FAZ COM QUE O FOR EXECUTE SOMENTE UMA VEZ, E PRINTA SOMENTE A ULTIMA LINHA
+    printf("x=%2d, y=%2d, z=%2d\n", x, y, z);
+    
+    
+    printf("------------------\n\n");
+
+}
