85c6c1c50e0239e697ecba870f0e745354ff8bcf
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 20:17:34 -0300

    Update update_stock.c

--- primeiro-semestre/disciplina-app/2022_06_07_update_stock/update_stock.c
+++ primeiro-semestre/disciplina-app/2022_06_07_update_stock/update_stock.c
@@ -1,3 +1,17 @@
+/**
+ * @file update_stock.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-07
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * Fazer um programa em c
+ * para cadstrar 5 produtos
+ * e atualizar o estoque.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
