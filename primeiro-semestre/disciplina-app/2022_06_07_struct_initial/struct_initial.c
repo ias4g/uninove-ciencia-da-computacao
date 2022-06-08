0e087ad0d6111f79ac88fbbd54e3e9b226a5deaa
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 22:02:49 -0300

    Update struct_initial.c

--- primeiro-semestre/disciplina-app/2022_06_07_struct_initial/struct_initial.c
+++ primeiro-semestre/disciplina-app/2022_06_07_struct_initial/struct_initial.c
@@ -44,3 +44,46 @@ int main()
     system("pause");
     return 0;
 }
+
+// #include<stdio.h>
+// #include<stdlib.h>
+// #include<locale.h>
+// #include<conio.h>
+
+// struct componente{
+	
+// 	char nome[20];
+// 	char referencia[5];
+// 	float preco;
+	
+// };
+
+
+
+// int main(){
+// 	system("color E");
+// 	setlocale(LC_ALL,"Portuguese");
+	
+// 	// tipo de dado   nome var
+// 	struct componente comp;
+	
+// 	printf("Nome do componente: ");
+// 	fgets(comp.nome,20,stdin);
+	
+// 	printf("Referencia: ");
+// 	fflush(stdin);
+// 	gets(comp.referencia);
+	
+// 	printf("Preço: ");
+// 	scanf("%f",&comp.preco);
+	
+// 	printf("Componente: %s\n",comp.nome);
+	
+	
+		
+   
+   
+// 	system("pause");
+// 	return 0;
+// }
+
