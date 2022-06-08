e86358ef39350bdf93586933fed9e266bc9e6be0
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 21:32:33 -0300

    Update struct_initial.c

--- primeiro-semestre/disciplina-app/2022_06_07_struct_initial/struct_initial.c
+++ primeiro-semestre/disciplina-app/2022_06_07_struct_initial/struct_initial.c
@@ -1,3 +1,13 @@
+/**
+ * @file struct_initial.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 0.1
+ * @date 2022-06-07
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
