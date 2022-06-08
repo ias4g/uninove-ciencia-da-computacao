5d86caf681094a2c4c007dc710ac6566b30bf264
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 21:17:35 -0300

    Update matrizes.c

--- primeiro-semestre/disciplina-app/2022_06_07_revisao/matrizes.c
+++ primeiro-semestre/disciplina-app/2022_06_07_revisao/matrizes.c
@@ -1,3 +1,14 @@
+/**
+ * @file matrizes.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 0.1
+ * @date 2022-06-07
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
@@ -21,8 +32,6 @@ int main()
 
     for (z = 0; x >= y; x = x - y, z++); // ESTE PONTO E VIRGULA FAZ COM QUE O FOR EXECUTE SOMENTE UMA VEZ, E PRINTA SOMENTE A ULTIMA LINHA
     printf("x=%2d, y=%2d, z=%2d\n", x, y, z);
-    
-    
-    printf("------------------\n\n");
 
+    printf("------------------\n\n");
 }
