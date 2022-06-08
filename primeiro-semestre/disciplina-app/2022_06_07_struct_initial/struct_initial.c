472854eef11b225d69caaf7e2308e5869d6674a6
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-07 21:31:53 -0300

    Create struct_initial.c

---
+++ primeiro-semestre/disciplina-app/2022_06_07_struct_initial/struct_initial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+
+struct struct_initial
+{
+    char nome[20];
+};
+
+int main()
+{
+    system("cls");
+    system("color A");
+
+    system("pause");
+    return 0;
+}
