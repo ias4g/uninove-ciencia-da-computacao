46c42500fc7410563d2816c6c6ffab88673be56d
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-14 19:40:33 -0300

    Create struct.c

---
+++ primeiro-semestre/disciplina-app/2022_06_14_struct/struct.c
@@ -0,0 +1,24 @@
+/**
+ * @brief
+ * @version 1.0
+ * @file struct.c
+ * @date 14 de Junho de 2022
+ * @copyright Copyright (c) 2022
+ *
+ * @author Izael Silva (https://github.com/Ias4g)
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    system("cls");
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    system("pause");
+    return 0;
+}
