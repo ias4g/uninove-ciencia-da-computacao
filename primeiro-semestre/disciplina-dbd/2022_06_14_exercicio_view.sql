186585f6a28a576dfc75d1bfbc8a5c6266b2c853
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-15 19:45:02 -0300

    Create 2022_06_14_exercicio_view.sql

---
+++ primeiro-semestre/disciplina-dbd/2022_06_14_exercicio_view.sql
@@ -0,0 +1,6 @@
+-- 001) Crie uma visãoda tabela EMP, chamada EMP com os seguintes campos,
+-- numero do departamento, media salarial, menor salario e maior salario.
+
+-- 002) Visualize a VIEW criada.
+
+-- 003) Apague a VIEW criada no exercicio anterior (VEMP)
