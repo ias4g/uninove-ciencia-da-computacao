f1d02686b07e2199e3b1b5557f4f852eb45eacc0
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-15 19:58:48 -0300

    Update 2022_06_14_exercicio_view.sql

--- primeiro-semestre/disciplina-dbd/2022_06_14_exercicio_view.sql
+++ primeiro-semestre/disciplina-dbd/2022_06_14_exercicio_view.sql
@@ -1,17 +1,20 @@
--- 001) Crie uma visãoda tabela EMP, chamada EMP com os seguintes campos,
--- numero do departamento, media salarial, menor salario e maior salario.
+--> 001) Crie uma visãoda tabela EMP, chamada EMP com os seguintes campos,
+--> numero do departamento, media salarial, menor salario e maior salario.
 CREATE OR REPLACE VIEW VEMP
 AS
 SELECT    DEPTNO COD, AVG(SAL) MED_SAL, MIN(SAL) SAL_MIN, MAX(SAL) SAL_MAX
 FROM      EMP
 GROUP BY  DEPTNO;
 
--- 002) Visualize a VIEW criada.
-
--- 003) Apague a VIEW criada no exercicio anterior (VEMP)
+SELECT * FROM VEMP;
 
 
--- #################################################################
+--> 002) Visualize a VIEW criada.
+
+--> 003) Apague a VIEW criada no exercicio anterior (VEMP)
+
+
+--........................................................................
 
 SELECT    DEPTNO, ROUND(AVG(SAL),2), MIN(SAL), MAX(SAL)
 FROM      EMP
