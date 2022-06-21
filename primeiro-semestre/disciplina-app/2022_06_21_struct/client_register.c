27565b65de8cfa9d28d634c4882acaf4fa3bed0b
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-21 19:44:16 -0300

    Create client_register.c

---
+++ primeiro-semestre/disciplina-app/2022_06_21_struct/client_register.c
@@ -0,0 +1,26 @@
+/**
+ * @file client_register.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 1.0
+ * @date 21 de Junho de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <conio.h>
+#include <string.h>
+
+int main()
+{
+    system('cls');
+    system('color A');
+    setlocale(LC_ALL, 'Portuguese');
+
+    system('pause');
+    return 0;
+}
