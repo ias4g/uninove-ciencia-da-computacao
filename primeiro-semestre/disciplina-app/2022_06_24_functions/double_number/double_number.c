6074db9675e7af050f9aad09acf4556a2279435f
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-24 20:13:12 -0300

    Create double_number.c

---
+++ primeiro-semestre/disciplina-app/2022_06_24_functions/double_number/double_number.c
@@ -0,0 +1,28 @@
+/**
+ * @file double_number.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 1.0
+ * @date 24 de Junho de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ * Faça um programa em c, que leia 3 numeros
+ * pra cada um imprimir o dobro
+ * o calculo deve ser realizado por uma função
+ * e o resultado apresentado no final no programa
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    system("cls");
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    system("pause");
+    return 0;
+}
