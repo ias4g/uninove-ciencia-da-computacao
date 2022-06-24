469a54f2259f9eaf30091fe608f8c2d074531449
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-24 20:47:51 -0300

    Create rectangle.c

---
+++ primeiro-semestre/disciplina-app/2022_06_24_functions/area_rectangle/rectangle.c
@@ -0,0 +1,30 @@
+/**
+ * @file rectangle.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 1.0
+ * @date 24 de Junho de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ * Calculando a area
+ * de um retangulo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+float area_rectangle()
+{
+}
+
+int main()
+{
+    system("cls");
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    system("pause");
+    return 0;
+}
