335115c7f953dfde847a5c2466b199c95be4e766
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-24 19:34:57 -0300

    Create functions.c

---
+++ primeiro-semestre/disciplina-app/2022_06_24_functions/functions.c
@@ -0,0 +1,24 @@
+/**
+ * @file functions.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 1.0
+ * @date 24 de Junho de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    system('cls');
+    system('color A');
+    setlocale(LC_ALL, 'Portuguese');
+
+    system('pause');
+    return 0;
+}
