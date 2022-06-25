9ead75812b94e3c95853dfece9709328708fc63b
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-24 21:25:37 -0300

    Update media.c

---
+++ primeiro-semestre/disciplina-app/2022_06_24_functions/calculating_media/media.c
@@ -0,0 +1,29 @@
+/**
+ * @file media.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 1.0
+ * @date 24 de Junho de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+float calculating_media(float p1, float p2, float p3)
+{
+}
+
+int main()
+{
+    system("cls");
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    system("pause");
+    return 0;
+}
