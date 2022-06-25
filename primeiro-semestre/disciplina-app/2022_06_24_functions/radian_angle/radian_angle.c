3026a4ac333518ea57276a4f030041323a36f57c
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-24 21:46:03 -0300

    Update radian_angle.c

--- primeiro-semestre/disciplina-app/2022_06_24_functions/radian_angle/radian_angle.c
+++ primeiro-semestre/disciplina-app/2022_06_24_functions/radian_angle/radian_angle.c
@@ -7,11 +7,22 @@
  *
  * @copyright Copyright (c) 2022
  *
+ * Faça um programa em C
+ * que leia o valor de um angulo em graus
+ * e o converta, ultilizando uma função
+ * para radiano e no final imprimir o resultado.
+ * Formula: rad = (ang * pi) / 180
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <math.h>
+
+float radian_angle(int valor)
+{
+    return (valor * 3.14) / 180;
+}
 
 int main()
 {
