02c652ec5176afd63ef1efbceb9c9a7a104869b9
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-28 19:58:30 -0300

    Update prime_number.c

---
+++ primeiro-semestre/disciplina-app/2022_06_28_exercise/prime_number/prime_number.c
@@ -0,0 +1,29 @@
+/**
+ * @file prime_number.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 1.0
+ * @date 28 de Junho de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int prime_number(int number)
+{
+    int i;
+}
+
+int main()
+{
+    system("cls");
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    return 0;
+}
