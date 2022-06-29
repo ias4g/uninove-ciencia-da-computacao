2c933310b9bc1b4ea9222b54b61218a12d702d8e
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-06-29 17:33:46 -0300

    Create exercicio_07.c

---
+++ primeiro-semestre/disciplina-app/2022_06_29_exercicio_07/exercicio_07.c
@@ -0,0 +1,34 @@
+/**
+ * @file exercicio_07.c
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @brief
+ * @version 1.0
+ * @date 29 de Junho de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ * Faça um programa que leia a base e a altura de um retângulo e,
+ * imprima o perímetro, a área e a diagonal.
+ * Para fazer os cálculos, implemente três funções,
+ * cada uma deve realizar um cálculo especifico conforme solicitado.
+ * Utilize as fórmulas a seguir.
+ * perimetro = 2x (base + altura)
+ * area = base x altura
+ * diagonal Vbase2+ altura2
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <math.h>
+
+int main()
+{
+    system("cls");
+    system("color A");
+    setlocale(LC_ALL, "Portuguese");
+
+    system("pause");
+    return 0;
+}
