66efdcd7959ac21efcf8932df8bf99807399276a
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-18 21:14:15 -0300

    Update Social.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_18_exercicio/Social.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_18_exercicio/Social.java
@@ -22,15 +22,15 @@ public class Social {
 
         // imprimindo os valores lidos
         if (salario <= 2424) {
-            JOptionPane.showMessageDialog(null, "Classe E" + salario);
+            JOptionPane.showMessageDialog(null, "Classe E - Salario: " + salario);
         } else if (salario > 2424 && salario <= 4848) {
-            JOptionPane.showMessageDialog(null, "Classe D" + salario);
+            JOptionPane.showMessageDialog(null, "Classe D - Salario: " + salario);
         } else if (salario > 4848 && salario <= 12120) {
-            JOptionPane.showMessageDialog(null, "Classe C" + salario);
+            JOptionPane.showMessageDialog(null, "Classe C - Salario: " + salario);
         } else if (salario > 12120 && salario <= 24240) {
-            JOptionPane.showMessageDialog(null, "Classe B" + salario);
+            JOptionPane.showMessageDialog(null, "Classe B - Salario: " + salario);
         } else {
-            JOptionPane.showMessageDialog(null, "Classe A" + salario);
+            JOptionPane.showMessageDialog(null, "Classe A - Salario: " + salario);
         }
     }
 }
