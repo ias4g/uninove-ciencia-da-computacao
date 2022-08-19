7366bcd936c951c954036a953fc0e9f5e6a3c255
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-18 21:12:18 -0300

    Update Social.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_18_exercicio/Social.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_18_exercicio/Social.java
@@ -1,3 +1,13 @@
+
+/**
+ * @file Social.java
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @date 18 de Agosto de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 import javax.swing.JOptionPane;
 
 public class Social {
@@ -8,17 +18,19 @@ public class Social {
         // armazenando os valores nas variavéis
         salario = Double.parseDouble(JOptionPane.showInputDialog("Qual é o seu salario?"));
 
+        salario *= 1.30;
+
         // imprimindo os valores lidos
         if (salario <= 2424) {
-            JOptionPane.showMessageDialog(null, "Classe E");
+            JOptionPane.showMessageDialog(null, "Classe E" + salario);
         } else if (salario > 2424 && salario <= 4848) {
-            JOptionPane.showMessageDialog(null, "Classe D");
+            JOptionPane.showMessageDialog(null, "Classe D" + salario);
         } else if (salario > 4848 && salario <= 12120) {
-            JOptionPane.showMessageDialog(null, "Classe C");
+            JOptionPane.showMessageDialog(null, "Classe C" + salario);
         } else if (salario > 12120 && salario <= 24240) {
-            JOptionPane.showMessageDialog(null, "Classe B");
+            JOptionPane.showMessageDialog(null, "Classe B" + salario);
         } else {
-            JOptionPane.showMessageDialog(null, "Classe A");
+            JOptionPane.showMessageDialog(null, "Classe A" + salario);
         }
     }
 }
