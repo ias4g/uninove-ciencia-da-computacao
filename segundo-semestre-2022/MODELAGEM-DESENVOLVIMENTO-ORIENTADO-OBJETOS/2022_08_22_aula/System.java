1d06e9c46f127632c9633ff67f45d683f17f7ed2
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-22 20:09:00 -0300

    Update System.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_22_aula/System.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_22_aula/System.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 /**
  * @file System.java
  * @author Izael Silva (https://github.com/Ias4g)
@@ -14,6 +16,6 @@ public class System {
         double salary;
 
         // Cadastrar as informações nas variáveis
-        
+        name = JOptionPane.showInputDialog("Digite seu nome");
     }
 }
