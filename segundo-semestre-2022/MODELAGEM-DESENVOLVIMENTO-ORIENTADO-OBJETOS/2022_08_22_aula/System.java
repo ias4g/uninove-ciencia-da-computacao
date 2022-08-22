542989ac26b7eac937c0f494813fede3af5f5f50
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-22 19:55:19 -0300

    Create System.java

---
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_22_aula/System.java
@@ -0,0 +1,14 @@
+/**
+ * @file System.java
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @date 22 de Agosto de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+public class System {
+    public static void main(String[] args) {
+
+    }
+}
