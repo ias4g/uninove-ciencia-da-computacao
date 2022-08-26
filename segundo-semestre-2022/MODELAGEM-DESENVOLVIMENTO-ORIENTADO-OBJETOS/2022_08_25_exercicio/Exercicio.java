b0c3b8d478e5bbd2fcbad67668dbcd6ac68d9118
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-25 21:10:20 -0300

    Update Exercicio.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_25_exercicio/Exercicio.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_25_exercicio/Exercicio.java
@@ -6,12 +6,16 @@ import javax.swing.JOptionPane;
  * @date 25 de Agosto de 2022
  *
  * @copyright Copyright (c) 2022
- *            Declarar uma variável chamada percentual
- *            Perguntar o percentual de aumento que se deseja aplicar no salário
- *            Passar para o método reajustarSalario o salario e o percentual
- *            Aplicar o aumento de acordo com o percentual fornecido
- *            Retornar o salário reajustado
- *            Exibir o salário reajustado
+ * 
+ * Declarar uma variável chamada percentual
+ * Perguntar o percentual de aumento que se
+ * deseja aplicar no salário
+ * Passar para o método reajustarSalario o
+ * salario e o percentual
+ * Aplicar o aumento de acordo com o
+ * percentual fornecido
+ * Retornar o salário reajustado
+ * Exibir o salário reajustado
  */
 
 public class Exercicio {
