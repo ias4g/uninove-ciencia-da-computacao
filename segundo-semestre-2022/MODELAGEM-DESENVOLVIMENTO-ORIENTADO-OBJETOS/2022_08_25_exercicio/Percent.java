a52804b3b4d24fecdf672fdcc8470254bc9e667e
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-25 21:24:08 -0300

    Update Percent.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_25_exercicio/Percent.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_25_exercicio/Percent.java
@@ -8,8 +8,22 @@
  */
 
 public class Percent {
-    public static double reajustarSalario(double salary, double percent) {
+    public static double reajustarsalary(double salary, double percent) {
         salary = salary + (salary * (percent / 100));
         return salary;
+    }
+
+    public static String verifyClass(double salary) {
+        if (salary <= 2424) {
+            return "Classe E";
+        } else if (salary <= 4848) {
+            return "Classe D";
+        } else if (salary <= 12120) {
+            return "Classe C";
+        } else if (salary <= 24240) {
+            return "Classe B";
+        } else {
+            return "Classe A";
+        }
     }
 }
