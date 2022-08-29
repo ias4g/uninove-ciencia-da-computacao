5ac76a457c6af0c81af5c42cba030e4b0bdb8603
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-29 19:47:01 -0300

    Create Teste.java

---
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_29_aula/Teste.java
@@ -0,0 +1,14 @@
+/**
+ * @file Teste.java
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @date 29 de Agosto de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+public class Teste {
+    public static void main(String[] args) {
+        
+    }
+}
