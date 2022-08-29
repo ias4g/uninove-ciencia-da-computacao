36516ffa8c5858730a64f8e0b7c48a76eb808a87
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-08-29 20:47:10 -0300

    Create MathOperations.java

---
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_08_29_aula/MathOperations.java
@@ -0,0 +1,14 @@
+/**
+ * @file MathOperations.java
+ * @author Izael Silva (https://github.com/Ias4g)
+ * @date 29 de Agosto de 2022
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+public class MathOperations {
+    public static double sum(double n1, double n2, int n3) {
+        return n1 + n2 + n3;
+    }
+}
