825c17b35cbcdfed1294f096e79bd521201ac755
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-09-06 18:52:10 -0300

    Update FormLogin.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_09_05_exercicio_form_login/src/screem/FormLogin.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/2022_09_05_exercicio_form_login/src/screem/FormLogin.java
@@ -1,3 +1,12 @@
+/**
+ * Exercício:
+ *
+ * Crie um formulário que tenha os campos, "USUÁRIO" e "SENHA", os botões "ENTRAR"
+ * e "CADASTRE-SE".
+ *
+ *
+ */
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -5,6 +14,7 @@
 package screem;
 
 /**
+ * fdfdfd
  *
  * @author ias4g
  */
