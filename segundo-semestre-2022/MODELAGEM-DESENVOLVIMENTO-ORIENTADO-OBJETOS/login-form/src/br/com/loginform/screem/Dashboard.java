f0c3eccb57e2e919498c21b1b086ec56ef790e07
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-09-29 16:41:50 -0300

    Update Dashboard.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/screem/Dashboard.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/screem/Dashboard.java
@@ -1,5 +1,6 @@
 package br.com.loginform.screem;
 
+import br.com.loginform.utils.Fonts;
 import br.com.loginform.utils.Utilities;
 import java.awt.Color;
 
@@ -8,6 +9,7 @@ public class Dashboard extends javax.swing.JFrame {
     public Dashboard() {
         initComponents();
         customComponents();
+        setFonts();
     }
 
     private void customComponents() {
@@ -15,9 +17,18 @@ public class Dashboard extends javax.swing.JFrame {
         setBackground(new Color(0, 0, 0));
     }
 
+    private void setFonts() {
+        lblFaviconDescription.setFont(new Fonts().getPT_SANS_BOLD_12());
+    }
+
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+
+        lblFavicon = new javax.swing.JLabel();
+        lblFaviconDescription = new javax.swing.JLabel();
+        lblClose = new javax.swing.JLabel();
+        lblMinimize = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Tela Inicial");
@@ -30,10 +41,30 @@ public class Dashboard extends javax.swing.JFrame {
         setSize(new java.awt.Dimension(1024, 590));
         getContentPane().setLayout(null);
 
+        lblFavicon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/loginform/assets/favicon.png"))); // NOI18N
+        getContentPane().add(lblFavicon);
+        lblFavicon.setBounds(10, 10, 13, 16);
+
+        lblFaviconDescription.setText("Ecoleta | Dashboard");
+        getContentPane().add(lblFaviconDescription);
+        lblFaviconDescription.setBounds(30, 10, 110, 16);
+
+        lblClose.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/loginform/assets/close.png"))); // NOI18N
+        getContentPane().add(lblClose);
+        lblClose.setBounds(984, 0, 40, 20);
+
+        lblMinimize.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/loginform/assets/minimize.png"))); // NOI18N
+        getContentPane().add(lblMinimize);
+        lblMinimize.setBounds(944, 0, 40, 20);
+
         setSize(new java.awt.Dimension(1024, 590));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel lblClose;
+    private javax.swing.JLabel lblFavicon;
+    private javax.swing.JLabel lblFaviconDescription;
+    private javax.swing.JLabel lblMinimize;
     // End of variables declaration//GEN-END:variables
 }
