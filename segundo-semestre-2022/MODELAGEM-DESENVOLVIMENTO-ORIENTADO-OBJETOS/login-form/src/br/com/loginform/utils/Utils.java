b9409d258a1156f004615866e8cf46ddcdbe3450
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-09-29 15:20:43 -0300

    Generate Getters

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/utils/Utils.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/utils/Utils.java
@@ -6,6 +6,34 @@ import java.io.IOException;
 
 public class Utils {
 
+    public Font getPT_SANS_BOLD_12() {
+        return PT_SANS_BOLD_12;
+    }
+
+    public Font getPT_SANS_BOLD_16() {
+        return PT_SANS_BOLD_16;
+    }
+
+    public Font getPT_SANS_REGULAR_12() {
+        return PT_SANS_REGULAR_12;
+    }
+
+    public Font getPT_SANS_REGULAR_16() {
+        return PT_SANS_REGULAR_16;
+    }
+
+    public Font getPT_SANS_REGULAR_24() {
+        return PT_SANS_REGULAR_24;
+    }
+
+    public Font getUBUNTU_BOLD_32() {
+        return UBUNTU_BOLD_32;
+    }
+
+    public Font getUBUNTU_BOLD_24() {
+        return UBUNTU_BOLD_24;
+    }
+
     private Font PT_SANS_BOLD_12;
     private Font PT_SANS_BOLD_16;
 
