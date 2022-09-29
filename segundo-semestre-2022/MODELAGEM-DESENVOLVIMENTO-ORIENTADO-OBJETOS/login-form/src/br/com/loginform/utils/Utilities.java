5b93a04f00933bdfcea1496c86c94f615a8e28a2
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-09-29 16:02:02 -0300

    Update Utilities.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/utils/Utilities.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/utils/Utilities.java
@@ -5,9 +5,17 @@ import javax.swing.JFrame;
 
 public class Utilities {
 
-    public static void insertIcon(JFrame frm) {
+    public static void insertIconFrm(JFrame frm) {
         try {
-            frm.setIconImage(Toolkit.getDefaultToolkit().getImage("src/br/com/loginform/assets/logo-2.png"));
+            frm.setIconImage(Toolkit.getDefaultToolkit().getImage("src/br/com/loginform/assets/icon-system.png"));
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public static void insertIconDialog(JFrame dialog) {
+        try {
+            dialog.setIconImage(Toolkit.getDefaultToolkit().getImage("src/br/com/loginform/assets/icon-system.png"));
         } catch (Exception e) {
             System.out.println(e.toString());
         }
