4b7e5fd4a700e25bc2ca0bd418f3fc7ffdcb623d
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-09-29 21:32:36 -0300

    Update CreateNewUser.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/project-2022-09-19/src/CreateNewUser.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/project-2022-09-19/src/CreateNewUser.java
@@ -7,6 +7,10 @@ import javax.swing.JOptionPane;
 
 public class CreateNewUser extends javax.swing.JFrame {
 
+    public CreateNewUser() {
+        initComponents();
+    }
+
     public CreateNewUser(int id, String user, String password, String name, String lastname, String email, String job) {
         initComponents();
     }
