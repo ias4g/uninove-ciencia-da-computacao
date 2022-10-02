0671072c7931856a255e49167ebe66af12003c28
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-10-02 14:14:15 -0300

    Update FontManager.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/utils/FontManager.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/utils/FontManager.java
@@ -1,24 +1,45 @@
 package br.com.loginform.utils;
 
+import br.com.loginform.screem.SplashScreem;
 import java.awt.Font;
 import java.awt.FontFormatException;
+import java.awt.GraphicsEnvironment;
+import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class FontManager {
 
-    public Font loadFont(String pathFont, int typeFont, int sizeFont) {
+//    public Font loadFont(String pathFont, int typeFont, int sizeFont) {
+//
+//        Font myFont = null;
+//
+//        try {
+//
+//            myFont = Font.createFont(
+//                    Font.TRUETYPE_FONT,
+//                    getClass().getResourceAsStream(pathFont)
+//            ).deriveFont(typeFont, sizeFont);
+//
+//        } catch (FontFormatException | IOException ex) {
+//            System.out.println(ex);
+//        }
+//
+//        return myFont;
+//    }
+    public static final Font Loading(String Fontname, Float Fontsize) {
 
         Font myFont = null;
 
         try {
+            myFont = Font.createFont(Font.TRUETYPE_FONT, new File("src/br/com/loginform/fonts/" + Fontname)).deriveFont(Fontsize);
 
-            myFont = Font.createFont(
-                    Font.TRUETYPE_FONT,
-                    getClass().getResourceAsStream(pathFont)
-            ).deriveFont(typeFont, sizeFont);
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(myFont);
 
         } catch (FontFormatException | IOException ex) {
-            System.out.println(ex);
+            Logger.getLogger(SplashScreem.class.getName()).log(Level.SEVERE, null, ex);
         }
 
         return myFont;
