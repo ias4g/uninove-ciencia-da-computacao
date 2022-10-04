80ab12197e3823fc4fcc2ec3b134f5eb7effc9bc
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-10-03 21:42:53 -0300

    Updates

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/project-2022-09-19/src/UsersList.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/project-2022-09-19/src/UsersList.java
@@ -9,15 +9,41 @@ public class UsersList extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tblUsers = new javax.swing.JTable();
+
         setTitle("Relatórios de usuários");
         setMaximumSize(new java.awt.Dimension(640, 426));
         setMinimumSize(new java.awt.Dimension(640, 426));
         setPreferredSize(new java.awt.Dimension(640, 426));
         getContentPane().setLayout(null);
 
+        tblUsers.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Id", "Usuário", "Senha", "Nome", "Sobrenome", "E-mail", "Cargo"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane1.setViewportView(tblUsers);
+
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(22, 20, 600, 380);
+
         setSize(new java.awt.Dimension(656, 434));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable tblUsers;
     // End of variables declaration//GEN-END:variables
 }
