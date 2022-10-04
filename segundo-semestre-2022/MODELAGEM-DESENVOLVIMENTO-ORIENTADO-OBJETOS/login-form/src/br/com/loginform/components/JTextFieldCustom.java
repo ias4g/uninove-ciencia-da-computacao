7414ef5e0d25f8b0cce6f72f29564e0b8d6d19d9
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-10-04 15:41:42 -0300

    Updates

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/components/JTextFieldCustom.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/components/JTextFieldCustom.java
@@ -10,30 +10,32 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 public class JTextFieldCustom extends JTextField {
-    
+
     private final Font PTSans_Regular_16 = FontManager.Loading("PTSans-Regular.ttf", Font.PLAIN, 16f);
     private final Color backgroundColor = new Color(244, 244, 244);
-    
+//    private final Color backgroundColor = Color.WHITE;
+
     public JTextFieldCustom() {
-        setBackground(new Color(244, 244, 244));
+        setBackground(new Color(244, 244, 244, 0));
+//        setBackground(new Color(255, 255, 255, 0));
         setOpaque(false);
         setBorder(new EmptyBorder(10, 10, 10, 50));
         setFont(PTSans_Regular_16);
+//        setFont(new java.awt.Font("sansserif", 0, 14));
         setSelectionColor(new Color(80, 199, 255));
     }
-    
+
     @Override
-    public void paintComponents(Graphics g) {
+    protected void paintComponent(Graphics g) {
         int width = getWidth();
         int height = getHeight();
-        
+
         Graphics2D g2 = (Graphics2D) g;
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
         g2.setColor(backgroundColor);
-        g2.fillRoundRect(0, 0, width, height, height, height);
-        
-        super.paintComponents(g);
+        g2.fillRoundRect(0, 0, width, height, 16, 16);
+
+        super.paintComponent(g);
     }
-    
 }
