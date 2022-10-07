12d8ddb36e0ba1185b6f4e7179d291231541f11b
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-10-06 22:00:31 -0300

    Update Menu.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/project-2022-09-19/src/Menu.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/project-2022-09-19/src/Menu.java
@@ -164,51 +164,53 @@ public class Menu extends javax.swing.JFrame {
                 "Digite o nome do usuário a ser excluido!"
         );
 
-        try {
-            //2 - Conectar no banco de dados sistemabd;
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/bdsystem", "student", "Izael@student"
-            );
+        System.out.println(u);
 
-            PreparedStatement st = conn.prepareStatement(
-                    "SELECT * FROM tbusers WHERE user = ?"
-            );
+        if (u == null) {
+            JOptionPane.showMessageDialog(null, "Preencha o nome do usuário!");
+        } else {
+            try {
+                //2 - Conectar no banco de dados sistemabd;
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/bdsystem", "student", "Izael@student"
+                );
 
-            st.setString(1, u);
-            ResultSet resultado = st.executeQuery();
+                PreparedStatement st = conn.prepareStatement(
+                        "SELECT * FROM tbusers WHERE user = ?"
+                );
 
-            if (u.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "Preencha o nome do usuário!");
+                st.setString(1, u);
+                ResultSet resultado = st.executeQuery();
+
+                if (resultado.next()) {
+                    int id = Integer.parseInt(resultado.getString("id"));
+                    String user = resultado.getString("user");
+                    String password = resultado.getString("password");
+                    String name = resultado.getString("name");
+                    String lastname = resultado.getString("lastname");
+                    String email = resultado.getString("email");
+                    String job = resultado.getString("job");
+
+                    //Abrir o formulário Menu.java
+                    new CreateNewUser(
+                            id, user, password, name, lastname, email, job
+                    ).setVisible(true);
+
+                } else {
+                    JOptionPane.showMessageDialog(null, "Usuário não encontrado");
+                }
+
+                //5 - Desconectar.
+                conn.close();
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Driver não está na library");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(
+                        null, "Você errou nos dados da conexão com o banco de dados"
+                );
             }
-
-            if (resultado.next()) {
-                int id = Integer.parseInt(resultado.getString("id"));
-                String user = resultado.getString("user");
-                String password = resultado.getString("password");
-                String name = resultado.getString("name");
-                String lastname = resultado.getString("lastname");
-                String email = resultado.getString("email");
-                String job = resultado.getString("job");
-
-                //Abrir o formulário Menu.java
-                new CreateNewUser(
-                        id, user, password, name, lastname, email, job
-                ).setVisible(true);
-
-            } else {
-                JOptionPane.showMessageDialog(null, "Usuário não encontrado");
-            }
-
-            //5 - Desconectar.
-            conn.close();
-
-        } catch (ClassNotFoundException ex) {
-            JOptionPane.showMessageDialog(null, "Driver não está na library");
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(
-                    null, "Você errou nos dados da conexão com o banco de dados"
-            );
         }
     }//GEN-LAST:event_itmDeleteUserActionPerformed
 
