227d53de1da397af5b18cf2f347d2cddc92e3a7e
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-10-09 19:49:39 -0300

    Update AddressController.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/controllers/AddressController.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/login-form/src/br/com/loginform/controllers/AddressController.java
@@ -1,18 +1,62 @@
 package br.com.loginform.controllers;
 
 import br.com.loginform.dao.DBConnection;
+import br.com.loginform.model.Address;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class AddressController {
-
+    
     private final Connection conn;
     private PreparedStatement stmt;
     private ResultSet rs;
-
+    
     public AddressController(Connection conn) {
         this.conn = DBConnection.getConn();
     }
-
+    
+    public String createAddress(Address address) {
+        if (conn != null) {
+            
+            try {
+                String sql = "INSERT INTO tb_address(zipcode, number, uf, city, point_id) values(?, ?, ?, ?, ?)";
+                
+                conn.setAutoCommit(false);
+                
+                stmt = conn.prepareStatement(sql);
+                stmt.setString(1, address.getZipcode());
+                stmt.setString(2, String.valueOf(address.getNumber()));
+                stmt.setString(3, address.getUf());
+                stmt.setString(4, address.getCity());
+                stmt.setString(5, String.valueOf(address.getPointId()));
+                
+                if (stmt.executeUpdate() == 1) {
+                    
+                    String getLastedId = "SELECT MAX(id) as id FROM tb_points";
+                    
+                    stmt = conn.prepareStatement(getLastedId);
+                    rs = stmt.executeQuery();
+                    
+                    while (rs.next()) {
+                        a.add(rs.getInt("id"));
+                    }
+                }
+                
+                conn.commit();
+                
+                return a;
+                
+            } catch (SQLException ex) {
+                return "=> Erro de SQL na class createPoint.\n=>Error: " + ex.getMessage();
+            } finally {
+                DBConnection.closeConn();
+            }
+            
+        } else {
+            return "=> Erro na tentativa de cadastrar o ponto, verifique a conexão com o BD.";
+        }
+    }
+    
 }
