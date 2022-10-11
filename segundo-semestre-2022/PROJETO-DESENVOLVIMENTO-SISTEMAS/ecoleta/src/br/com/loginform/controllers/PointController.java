18593cb658c3f6df5814292759b24fa53a4eae36
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-10-11 15:04:29 -0300

    updates

--- segundo-semestre-2022/PROJETO-DESENVOLVIMENTO-SISTEMAS/ecoleta/src/br/com/loginform/controllers/PointController.java
+++ segundo-semestre-2022/PROJETO-DESENVOLVIMENTO-SISTEMAS/ecoleta/src/br/com/loginform/controllers/PointController.java
@@ -10,132 +10,107 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PointController {
-
+    
     private final Connection conn;
     private PreparedStatement stmt;
     private ResultSet rs;
-
+    
     public PointController() {
         this.conn = DBConnection.getConn();
     }
-
-    public ArrayList createPoint(Point point) {
-
-        ArrayList<Object> a = new ArrayList();
-
+    
+    public String createPoint(Point point) {
+        
         if (conn != null) {
-
+            
             try {
-
+                
                 String sql = "INSERT INTO tb_points(id, name, email, image, whatsapp) values(?, ?, ?, ?, ?)";
-
+                
                 stmt = conn.prepareStatement(sql);
-
+                
                 stmt.setString(1, point.getId());
                 stmt.setString(2, point.getName());
                 stmt.setString(3, point.getEmail());
                 stmt.setBytes(4, point.getImage());
                 stmt.setString(5, point.getWhatsapp());
-
-                if (stmt.executeUpdate() == 1) {
-
-                    String getLastedId = "SELECT MAX(id) as id FROM tb_points";
-
-                    stmt = conn.prepareStatement(getLastedId);
-                    rs = stmt.executeQuery();
-
-                    while (rs.next()) {
-                        a.add(rs.getInt("id"));
-                    }
-                }
-
-                return a;
-
+                
+                return String.valueOf(stmt.executeUpdate());
+                
             } catch (SQLException ex) {
-//                try {
-//                    conn.rollback();
-//
-//                } catch (SQLException ex1) {
-//                    a.add(ex1.getMessage());
-//                    return a;
-//                }
-
-                a.add("=> Erro de SQL na class createPoint.\n=>Error: " + ex.getMessage());
-                return a;
-
+                return "=> Erro de SQL na class createPoint.\n=>Error: " + ex.getMessage();
             } finally {
                 DBConnection.closeConn();
             }
         } else {
-            a.add("=> Erro na tentativa de cadastrar o ponto, verifique a conexão com o BD.");
-            return a;
+            return "=> Erro na tentativa de cadastrar o ponto, verifique a conexão com o BD.";
         }
     }
-
+    
     public List<Point> ReadPoint() {
         String sql = "SELECT * FROM tb_points";
         List<Point> pt = new ArrayList<>();
-
+        
         try {
             stmt = conn.prepareStatement(sql);
             rs = stmt.executeQuery();
-
+            
             while (rs.next()) {
                 Point point = new Point();
-
+                
                 point.setId(rs.getString("id"));
                 point.setName(rs.getString("name"));
                 point.setEmail(rs.getString("email"));
                 point.setImage(rs.getBytes("image"));
                 point.setWhatsapp(rs.getString("whatsapp"));
-
+                
                 pt.add(point);
             }
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-
+        
         return pt;
     }
-
+    
     public void updatePoint(Point point) {
-
+        
         String sql = "UPDATE tb_points name = ?, email = ?, image = ?, whatsapp = ? WHERE id = ?)";
-
+        
         try {
-
+            
             stmt = conn.prepareStatement(sql);
-
+            
             stmt.setString(1, point.getName());
             stmt.setString(2, point.getEmail());
             stmt.setBytes(3, point.getImage());
             stmt.setString(4, point.getWhatsapp());
-
+            
             stmt.setString(5, point.getId());
-
+            
             stmt.executeUpdate();
-
+            
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-
+        
     }
-
+    
     public void deletePoint(Point point) {
-
+        
         String sql = "DELETE FROM tb_points WHERE id = ?";
-
+        
         try {
-
+            
             stmt = conn.prepareStatement(sql);
             stmt.setString(1, point.getId());
-
+            
             stmt.executeUpdate();
-
+            
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-
+        
     }
-
+    
 }
