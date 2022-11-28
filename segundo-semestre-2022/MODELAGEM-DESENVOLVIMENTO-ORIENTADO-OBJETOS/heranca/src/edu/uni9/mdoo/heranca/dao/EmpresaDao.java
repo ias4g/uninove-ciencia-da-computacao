5b83b66a0bc5ad46b27cf1c6c3550e0d5b9a5f1a
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-11-28 20:57:52 -0300

    Updates

---
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/heranca/src/edu/uni9/mdoo/heranca/dao/EmpresaDao.java
@@ -0,0 +1,22 @@
+package edu.uni9.mdoo.heranca.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class EmpresaDao {
+
+    private Connection conn = null;
+    private PreparedStatement stmt = null;
+    
+    private final String BD_USER = "student";
+    private final String BD_PASSWORD = "Izael@student";
+    private final String BD_URL = "jdbc:mysql://localhost:3306/emp_her";
+
+    public void getConn() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        conn = DriverManager.getConnection(BD_URL, BD_USER, BD_PASSWORD);
+    }
+
+}
