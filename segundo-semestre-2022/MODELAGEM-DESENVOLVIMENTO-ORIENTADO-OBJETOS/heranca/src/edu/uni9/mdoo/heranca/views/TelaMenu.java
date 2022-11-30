ca949a375b1094309a6a8be75b44e3022f7e16e0
Izael silva <62667424+Ias4g@users.noreply.github.com>
2022-11-30 18:35:06 -0300

    Update TelaMenu.java

--- segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/heranca/src/edu/uni9/mdoo/heranca/views/TelaMenu.java
+++ segundo-semestre-2022/MODELAGEM-DESENVOLVIMENTO-ORIENTADO-OBJETOS/heranca/src/edu/uni9/mdoo/heranca/views/TelaMenu.java
@@ -95,10 +95,12 @@ public class TelaMenu extends javax.swing.JFrame {
 
         String op = pergunta("O que você quer fazer?", "Escolha uma opção abaixo!");
 
-        if (op.equalsIgnoreCase("Cadastrar")) {
-            new FrmFuncionario(this, true).setVisible(true);
-        } else if (op.equalsIgnoreCase("Relatório")) {
+        if (op != null) {
+            if (op.equalsIgnoreCase("Cadastrar")) {
+                new FrmFuncionario(this, true).setVisible(true);
+            } else if (op.equalsIgnoreCase("Relatório")) {
 
+            }
         }
 
     }//GEN-LAST:event_btnFuncionarioActionPerformed
