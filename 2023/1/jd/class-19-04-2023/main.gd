5d9746895eac7f1b073f71d888cf6b6bb7f8e766
Izael silva <62667424+Ias4g@users.noreply.github.com>
2023-04-19 21:10:09 -0300

    Update main.gd

--- 2023/1/jd/class-19-04-2023/main.gd
+++ 2023/1/jd/class-19-04-2023/main.gd
@@ -1,48 +1,13 @@
 extends Node
 
-var cor = "" setget set_cor, get_cor
-var modelo = "" setget set_modelo, get_modelo
-var marca = "" setget set_marca, get_marca
-
-# Prata, i30, hyundai
-# STAR VARIABLE COR -------------------------------------
-func set_cor(color):
-	cor = color
-	pass
-
-func get_cor():
-	return cor
-	pass
-# END VARIABLE COR -------------------------------------
-
-# STAR VARIABLE MODELO -------------------------------------
-func set_modelo(mod):
-	modelo = mod
-	pass
-
-func get_modelo():
-	return modelo
-	pass
-# END VARIABLE MODELO -------------------------------------
-
-# STAR VARIABLE MARCA -------------------------------------
-func set_marca(mar):
-	marca = mar
-	pass
-
-func get_marca():
-	return marca
-	pass
-# END VARIABLE MARCA -------------------------------------
+class ObjectScene:
+	var height = 10
+	var width = 3
+	var color = "Yellow"
 
 func _ready():
-	# set - setandoos valores
-	self.set_cor("Prata")
-	self.set_modelo("i30")
-	self.set_marca("Hyundai")
-	
-	# get - pegar os valores
-	print(self.get_cor())
-	print(self.get_modelo())
-	print(self.get_marca())
+	var obj = ObjectScene.new()
+	print(obj.height)
+	print(obj.width)
+	print(obj.color)
 	pass
