0478848eabca4c23bf1190b89af199520a8f75f9
Izael silva <62667424+Ias4g@users.noreply.github.com>
2023-04-19 21:29:06 -0300

    Update main.gd

--- 2023/1/jd/class-19-04-2023/main.gd
+++ 2023/1/jd/class-19-04-2023/main.gd
@@ -1,39 +1,22 @@
 extends Node
 
-class ObjectScene:
-	var height = 10
-	var width = 3
-	var color = "Yellow"
-	pass
-
-class Person:
-	var name = "Izael Silva" setget set_name, get_name
-	var email = "person@person.com.br" setget set_email, get_email
+class Hero:
+	var name = "" setget set_name, get_name
 	
-	func set_name(mn):
-		name = mn
+	func set_name(var nm):
+		name = nm
 		pass
 	
 	func get_name():
 		return name
 		pass
-	
-	func set_email(em):
-		email = em
-		pass
-	
-	func get_email():
-		return email
-		pass
-	pass
 
 func _ready():
-	var p = Person.new()
-	print(p.name)
-	print(p.email)
-
-	var obj = ObjectScene.new()
-	print(obj.height)
-	print(obj.width)
-	print(obj.color)
+	var h1 = Hero.new()
+	h1.name = "Superman"
+	print(h1.name)
+	
+	var h2 = Hero.new()
+	h2.name = "Sonic"
+	print(h2.name)
 	pass
