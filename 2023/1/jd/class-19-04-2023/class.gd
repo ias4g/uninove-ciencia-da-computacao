44d98c2a8668badedc4f2d40ceb48de46b8fc2aa
Izael silva <62667424+Ias4g@users.noreply.github.com>
2023-04-19 21:19:41 -0300

    Update class.gd

--- 2023/1/jd/class-19-04-2023/class.gd
+++ 2023/1/jd/class-19-04-2023/class.gd
@@ -4,8 +4,35 @@ class ObjectScene:
 	var height = 10
 	var width = 3
 	var color = "Yellow"
+	pass
+
+class Person:
+	var name = "Izael Silva" setget set_name, get_name
+	var email = "person@person.com.br" setget set_email, get_email
+	
+	func set_name(mn):
+		name = mn
+		pass
+	
+	func get_name():
+		return name
+		pass
+		
+	func set_email(em):
+		email = em
+		pass
+	
+	func get_email():
+		return email
+		pass
+	
+	pass
 
 func _ready():
+	var p = Person.new()
+	print(p.name)
+	print(p.email)
+
 	var obj = ObjectScene.new()
 	print(obj.height)
 	print(obj.width)
