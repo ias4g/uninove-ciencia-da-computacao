ae3370ebeaded92d672488cf788b4fea4393ac3a
Izael silva <izaell.oficial@gmail.com>
2024-04-13 22:02:53 -0300

    Create Form1.cs

---
+++ 2024/1/PV/activities/AULA-12-04-2024-MATRIZ/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+
+namespace AULA_12_04_2024_MATRIZ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnShow_Click(object sender, System.EventArgs e)
+        {
+            LbList.Items.Clear();
+
+            string[,] nomes = new string[,] {
+                { "Izael", "Proberto", "Love"},
+                { "Eversson", "Gabriel", "Jonas"},
+                { "Prof", "Jessica", "gabi"},
+                { "Deus", "Jesus", "Victoria"}
+            };
+
+            for (int i = 0; i < nomes.GetLength(0); i++)
+            {
+                for (int j = 0; j < nomes.GetLength(1); j++)
+                {
+                    LbList.Items.Add("NA POSIÇÃO [" + i + ", " + j + "] É: " + nomes[i, j]);
+                }
+            }
+
+            this.Text = "SUA MATRIZ É " + nomes.GetLength(0).ToString() + " por " + nomes.GetLength(1).ToString();
+        }
+    }
+}
