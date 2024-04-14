db9e6d972762961186b533a61ec9dddf51775393
Izael silva <izaell.oficial@gmail.com>
2024-04-13 22:02:57 -0300

    Create Form1.Designer.cs

---
+++ 2024/1/PV/activities/AULA-12-04-2024-MATRIZ/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace AULA_12_04_2024_MATRIZ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnShow = new System.Windows.Forms.Button();
+            this.LbList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MATRIZ";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnShow
+            // 
+            this.BtnShow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnShow.Location = new System.Drawing.Point(12, 35);
+            this.BtnShow.Name = "BtnShow";
+            this.BtnShow.Size = new System.Drawing.Size(360, 29);
+            this.BtnShow.TabIndex = 1;
+            this.BtnShow.Text = "MOSTAR RESULTADO";
+            this.BtnShow.UseVisualStyleBackColor = true;
+            this.BtnShow.Click += new System.EventHandler(this.BtnShow_Click);
+            // 
+            // LbList
+            // 
+            this.LbList.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbList.FormattingEnabled = true;
+            this.LbList.ItemHeight = 25;
+            this.LbList.Location = new System.Drawing.Point(15, 83);
+            this.LbList.Name = "LbList";
+            this.LbList.Size = new System.Drawing.Size(357, 454);
+            this.LbList.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.LbList);
+            this.Controls.Add(this.BtnShow);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MATRIZ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnShow;
+        private System.Windows.Forms.ListBox LbList;
+    }
+}
+
