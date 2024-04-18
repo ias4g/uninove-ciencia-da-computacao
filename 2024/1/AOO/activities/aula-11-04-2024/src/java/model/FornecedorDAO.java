e25784cfa030274fc4113f2a778e9bc3618e6587
Izael silva <izaell.oficial@gmail.com>
2024-04-18 19:42:30 -0300

    Update FornecedorDAO.java

--- 2024/1/AOO/activities/aula-11-04-2024/src/java/model/FornecedorDAO.java
+++ 2024/1/AOO/activities/aula-11-04-2024/src/java/model/FornecedorDAO.java
@@ -7,7 +7,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import model.bean.Cliente;
 import utils.ConnectionFactory;
 
 // COM BASE EM ClienteDAO, fazer FornecedorDAO
