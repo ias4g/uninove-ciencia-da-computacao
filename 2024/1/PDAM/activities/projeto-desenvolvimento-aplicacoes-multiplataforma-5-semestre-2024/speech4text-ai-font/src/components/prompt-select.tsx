5023c217b75358b5cc2288962ec6378353b99e59
Izael silva <izaell.oficial@gmail.com>
2024-06-04 21:15:45 -0300

    Create prompt-select.tsx

---
+++ 2024/1/PDAM/activities/projeto-desenvolvimento-aplicacoes-multiplataforma-5-semestre-2024/speech4text-ai-font/src/components/prompt-select.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import { api } from "@/lib/axios";
+
+interface Prompts {
+  id: string
+  title: string
+  template: string
+}
+
+interface PromptSelectProps {
+  onPromptSelected: (template: string) => void
+}
+
+export function PromptSelect({ onPromptSelected }: PromptSelectProps) {
+  const [prompts, setPrompts] = useState<Prompts[] | null>(null)
+
+  useEffect(() => {
+    api.get('/prompts').then(response => {
+      setPrompts(response.data)
+    })
+  }, [])
+
+  function handlePromptSelected(promptId: string) {
+    const selectedPrompt = prompts?.find(prompt => prompt.id === promptId)
+
+    if (!selectedPrompt) {
+      return
+    }
+
+    onPromptSelected(selectedPrompt.template)
+  }
+
+  return (
+    <Select onValueChange={handlePromptSelected}>
+      <SelectTrigger>
+        <SelectValue placeholder="Selecione um prompt..." />
+      </SelectTrigger>
+      <SelectContent>
+        {prompts?.map(prompt => {
+          return (
+            <SelectItem key={prompt.id} value={prompt.id}>
+              {prompt.title}
+            </SelectItem>
+          )
+        })}
+      </SelectContent>
+    </Select>
+  )
+}
