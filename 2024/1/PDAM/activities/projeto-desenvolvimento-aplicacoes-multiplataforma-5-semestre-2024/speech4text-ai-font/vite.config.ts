86c2197e221ef10b222b186002362b06ac7605e8
Izael silva <izaell.oficial@gmail.com>
2024-06-04 21:17:00 -0300

    Create vite.config.ts

---
+++ 2024/1/PDAM/activities/projeto-desenvolvimento-aplicacoes-multiplataforma-5-semestre-2024/speech4text-ai-font/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import path from "path"
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
+})
