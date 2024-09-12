7e34c0a2519164615b7040b31cf67602bd3116dc
Izael silva <izaell.oficial@gmail.com>
2024-09-11 21:43:39 -0300

    Update aula_10_09_2024.c

--- 2024/2/estrutura-de-dados/activities/aula_10_09_2024.c
+++ 2024/2/estrutura-de-dados/activities/aula_10_09_2024.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+void bolha(int *v, int tam)
 {
-  /* code */
+  int aux, troca;
+  int j = tam - 1;
+  int i;
+
+  do
+  {
+    troca = 0;
+    for (i = 0; i < j; i++)
+      if (v[i] > v[i + 1])
+      {
+        aux = v[i];
+        v[i] = v[i + 1];
+        v[i + 1] = aux;
+        troca = 1;
+      }
+    j--;
+  } while (troca);
+}
+
+int main(int argc)
+{
+  int v[] = {10, 2, 67, 34, 1};
+
+  bolha(v, 5);
   return 0;
 }
