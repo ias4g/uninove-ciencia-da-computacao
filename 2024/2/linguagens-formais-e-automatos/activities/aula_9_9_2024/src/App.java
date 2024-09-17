fde2ae53c1af02b8cb22ea8abc3264c5bb3b415c
Izael silva <izaell.oficial@gmail.com>
2024-09-17 20:11:47 -0300

    Update App.java

--- 2024/2/linguagens-formais-e-automatos/activities/aula_9_9_2024/src/App.java
+++ 2024/2/linguagens-formais-e-automatos/activities/aula_9_9_2024/src/App.java
@@ -17,26 +17,25 @@ public class App {
         String data = "99/99/9999";
         String cep = "02.969-130";
         String codigo = "XF1234545";
-        String nome = "Izael Silva";
-        // String email = "izael@gmail.com";
-        // String logradouro = "Jd. São José";
-        // String website = "https://www.google.com";
-
-        // String number = "123.456.789-01";
-        // boolean regex = number.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}");
+        String nome = "João sa silva";
+        String email = "izael@gmail.com";
+        String logradouro = "Jd. São José";
+        String website = "https://www.google.com";
 
         boolean regex_data = data.matches("^[0-9]{2}/[0-9]{2}/[0-9]{4}");
         boolean regex_cep = cep.matches("^[0-9]{2}\\.[0-9]{3}-[0-9]{3}");
         boolean regex_codigo = codigo.matches("^[A-Z]{2}+[0-9]{7}");
-        boolean regex_nome = nome.matches("^[a-zA-Z\s]+");
-        // boolean regex_email = email.matches("");
-        // boolean regex_logradouro = email.matches(number);
-        // boolean regex_website = website.matches("^[https://www.[a-z]+.com]");
+        boolean regex_nome = nome.matches("^[A-Za-zÀ-ÖØ-öø-ÿ]+(\\s[A-Za-zÀ-ÖØ-öø-ÿ]+)*");
+        boolean regex_email = email.matches("^[\\w._%+-]+@[\\w.-]+\\.[a-zA-Z]{3,}");
+        boolean regex_logradouro = logradouro.matches("^(Rua|Av\\.?|Avenida|Jd\\.?|Jardim|Travessa|Al\\.?|Alameda)? ?[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*");
+        boolean regex_website = website.matches("^(https?://)?(www\\.)?[a-zA-Z0-9-]+(\\.[a-zA-Z]{3,})");
 
-        System.out.printf("É valido DATA? %s\n\n", regex_data);
+        System.out.printf("\n\nÉ valido DATA? %s\n\n", regex_data);
         System.out.printf("É valido CEP? %s\n\n", regex_cep);
         System.out.printf("É valido CODIGO? %s\n\n", regex_codigo);
         System.out.printf("É valido nome? %s\n\n", regex_nome);
-        // System.out.printf("É valido website? %s\n\n", regex_website);
+        System.out.printf("É valido email? %s\n\n", regex_email);
+        System.out.printf("É valido logradouro? %s\n\n", regex_logradouro);
+        System.out.printf("É valido website? %s\n\n", regex_website);
     }
 }
